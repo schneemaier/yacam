e32aa0e1696be866901e4b66738b0884d5fa2bef
schneemaier <akos.schneemaier@gmail.com>
2023-09-17 14:07:13 -0400

    - removing unneeded mjpg streamer input and output options - adding back temporary the ww directory for mjpg streams - som elogging changes

--- custompackages/package/mjpg-streamer/mjpg-streamer.mk
+++ custompackages/package/mjpg-streamer/mjpg-streamer.mk
@@ -6,8 +6,17 @@ MJPG_STREAMER_DEPENDENCIES =
 MJPG_STREAMER_SUBDIR = mjpg-streamer-experimental
 
 define MJPG_STREAMER_TARGET_CLEANUP
-	rm -rf $(TARGET_DIR)/usr/share/mjpg-streamer/www
+	# remot WWW directory
+	#rm -rf $(TARGET_DIR)/usr/share/mjpg-streamer/www
+	# remove unused mjpg stream input and output options
+        rm -rf $(TARGET_DIR)/usr/lib/mjpg-streamer/input_file.so
+        rm -rf $(TARGET_DIR)/usr/lib/mjpg-streamer/input_http.so
+        rm -rf $(TARGET_DIR)/usr/lib/mjpg-streamer/input_uvc.so
+        rm -rf $(TARGET_DIR)/usr/lib/mjpg-streamer/output_file.so
+        rm -rf $(TARGET_DIR)/usr/lib/mjpg-streamer/output_rtsp.so
+        rm -rf $(TARGET_DIR)/usr/lib/mjpg-streamer/output_udp.so
 endef
+
 MJPG_STREAMER_POST_INSTALL_TARGET_HOOKS += MJPG_STREAMER_TARGET_CLEANUP
 
 $(eval $(cmake-package))
